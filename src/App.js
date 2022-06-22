eee2f3324fdd59f2787152b83a76f69240257056
Abdulrahman <rahbalood@gmail.com>
2022-06-22 21:58:37 +0300

    spinner added

--- src/App.js
+++ src/App.js
@@ -8,8 +8,10 @@ function App() {
   const [temperature, setTemperature] = useState("");
   const [wind, setWind] = useState("");
   const [description, setDescription] = useState("");
+  const [loading, setLoading] = useState(true);
 
   const onClick = async () => {
+    setLoading(true);
     const request = await fetch(
       "https://goweather.herokuapp.com/weather/" + country
     );
@@ -19,9 +21,11 @@ function App() {
     setTemperature(data.temperature);
     setWind(data.wind);
     setDescription(data.description);
+    setLoading(false);
   };
   useEffect(() => {
     const initialDate = async () => {
+      setLoading(true);
       const request = await fetch(
         "https://goweather.herokuapp.com/weather/saudi"
       );
@@ -31,18 +35,36 @@ function App() {
       setTemperature(data.temperature);
       setWind(data.wind);
       setDescription(data.description);
+      setLoading(false);
     };
     initialDate();
   }, []);
   return (
     <div className="App">
-      <DataComponent countryName={countryName} temperature={temperature}
-      wind={wind}
-      description={description}/>
-      <input
-        onChange={(e) => {setCountry(e.target.value); }}className="form-control mt-1"type="text"placeholder="Enter a country"aria-label="default input example"
-      />
-      <ButtonsComponent buttonName={"Search"} onClick={onClick}/>
+      {loading ? (
+        <div class="spinner-border" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+      ) : (
+        <>
+          <DataComponent
+            countryName={countryName}
+            temperature={temperature}
+            wind={wind}
+            description={description}
+          />
+          <input
+            onChange={(e) => {
+              setCountry(e.target.value);
+            }}
+            className="form-control mt-1"
+            type="text"
+            placeholder="Enter a country"
+            aria-label="default input example"
+          />
+          <ButtonsComponent buttonName={"Search"} onClick={onClick} />
+        </>
+      )}
     </div>
   );
 }
